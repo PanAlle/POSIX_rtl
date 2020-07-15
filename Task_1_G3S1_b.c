13f3c7fd222df64c43a7cbf4627b5d8849a37c86
PanAlle <65158179+PanAlle@users.noreply.github.com>
2020-07-15 14:22:38 +0200

    Base file Task 1
    Write a periodic real-time task that uses a POSIX timer and the signal it sends to implement a periodic task.Your task should output a warning if the deadline (equal to period) is missed.  Hint:  If a signal is received and the process is not blocking on sigwait, the registered signal handler is used instead

---
+++ Task_1_G3S1_b.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include "errno.h"
+#include <stdlib.h>
+#include <string.h>
+
+# define CLOCKID CLOCK_REALTIME
+
+void new_action_funct(){
+printf("Missed deadline \n");
+}
+
+
+int main(){
+
+sigset_t signal_set;
+timer_t timer;
+struct itimerspec timer_spec;
+struct sigaction new_action;
+memset(&new_action, 0, sizeof(new_action));
+// Create a timer.
+timer_create(CLOCK_REALTIME, NULL, &timer);
+
+// Specify the timer’s period.
+long period = 1;
+timer_spec.it_value.tv_sec = 0;
+timer_spec.it_value.tv_nsec = 1; /* Start immediately */
+timer_spec.it_interval.tv_sec = 5*period;
+timer_spec.it_interval.tv_nsec = 0;
+
+printf("Set up structure for timer \n");
+
+new_action.sa_handler = new_action_funct;
+printf("Defined the next handler \n");
+
+sigemptyset( &new_action.sa_mask );
+new_action.sa_flags = 0;
+sigaction(SIGALRM, &new_action, NULL);
+printf("Define a new sigaction \n");
+
+// Setup a signal set for sigwait() to wait for SIGALRM
+sigemptyset(&signal_set);
+sigaddset(&signal_set, SIGALRM);
+//sigprocmask(SIG_BLOCK, &signal_set, NULL);
+int signum;
+
+// Setup the timer’s period.
+timer_settime(timer, 0, &timer_spec, NULL);
+sleep(10);
+while (1){
+//		unsigned long mesg;
+	sigprocmask(SIG_BLOCK, &signal_set, NULL);
+	sigwait(&signal_set, &signum);
+	printf("--- Task completed ---\n");
+	// since we put SIGALRM into the blocked singal then  the next step 		we will stop on sigwait
+	}
+
+}
