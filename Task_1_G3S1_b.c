9a2349c808d6041f2ceebc1418b28ca63d8009bc
Alessandro Panini <ge97dil@mytum.de>
2020-07-16 15:57:27 +0200

    Added a loop funciton and removed sigprog mask. Now every 10 iteration a deadline is deployed

--- Task_1_G3S1_b.c
+++ Task_1_G3S1_b.c
@@ -25,37 +25,35 @@ timer_create(CLOCK_REALTIME, NULL, &timer);
 
 // Specify the timer’s period.
 long period = 1;
-timer_spec.it_value.tv_sec = 0;
-timer_spec.it_value.tv_nsec = 1; /* Start immediately */
-timer_spec.it_interval.tv_sec = 5*period;
+timer_spec.it_value.tv_sec = period;
+timer_spec.it_value.tv_nsec = 0; 
+timer_spec.it_interval.tv_sec = period;
 timer_spec.it_interval.tv_nsec = 0;
+printf("My pid:%d\n", getpid());
+printf("Set up struct for timer \n");
 
-printf("Set up structure for timer \n");
-
+//Give to sigaction the new handler
 new_action.sa_handler = new_action_funct;
-printf("Defined the next handler \n");
 
+//Clean the sigaction mask for blocked singals
 sigemptyset( &new_action.sa_mask );
 new_action.sa_flags = 0;
 sigaction(SIGALRM, &new_action, NULL);
-printf("Define a new sigaction \n");
+printf("Defined a new handler for SIGALRM \n");
 
 // Setup a signal set for sigwait() to wait for SIGALRM
 sigemptyset(&signal_set);
 sigaddset(&signal_set, SIGALRM);
+timer_settime(timer, 0, &timer_spec, NULL);
+
 //sigprocmask(SIG_BLOCK, &signal_set, NULL);
 int signum;
+int counter = 0;
 
-// Setup the timer’s period.
-timer_settime(timer, 0, &timer_spec, NULL);
-sleep(10);
-while (1){
-//		unsigned long mesg;
-	sigprocmask(SIG_BLOCK, &signal_set, NULL);
-	sleep(10);
+while(1){
+	if(counter%10 == 0) sleep(2);
 	sigwait(&signal_set, &signum);
 	printf("--- Task completed ---\n");
-	// since we put SIGALRM into the blocked singal then  the next step 		we will stop on sigwait
+	counter++;
 	}
-
 }
