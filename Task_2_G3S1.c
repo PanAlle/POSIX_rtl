6db25467b22087e195b673502e2c5363a7e3f90f
Alessandro Panini <ge97dil@mytum.de>
2020-07-21 18:56:52 +0200

    Added char input to better define FIFO and RR

--- Task_2_G3S1.c
+++ Task_2_G3S1.c
@@ -9,17 +9,15 @@
 
 # define CLOCKID CLOCK_REALTIME
 
+//Handler for SIGALRM
 void new_action_funct(){
 printf("Missed deadline \n");
 }
-
-
+//RT task
 void p_task(long period_ms, int load, timer_t timer, struct itimerspec time_left){
 for(int i = 0; i < load*1000; i++){}
-
 timer_gettime(timer, &time_left);
 float task_duration = (time_left.it_value.tv_sec*1000000000 + time_left.it_value.tv_nsec)/1000000.0;
-
 printf("Time used for execution in ms is %1.5f \n", period_ms - task_duration);
 }
 
@@ -29,9 +27,6 @@ int main(int argc, char** argv){
 sigset_t signal_set;
 timer_t timer;
 struct itimerspec timer_spec, time_left;
-//struct sigaction task;
-
-
 
 // Create a timer
 timer_create(CLOCK_REALTIME, NULL, &timer);
@@ -39,41 +34,47 @@ timer_create(CLOCK_REALTIME, NULL, &timer);
 // Specify the timer’s period.
 long period_ms = atoi(argv[1]);
 int load = atoi(argv[2]);
-printf("Started with a period in ms of:%ld\n", period_ms);
+
 timer_spec.it_value.tv_sec = 0;
 timer_spec.it_value.tv_nsec = period_ms*1000000; 
 timer_spec.it_interval.tv_sec = 0;
 timer_spec.it_interval.tv_nsec = period_ms*1000000;
-printf("My pid:%d\n", getpid());
 
-//New action
+//Process infos
+printf("PID:%d\n", getpid());
+printf("Clock period:%ld ms\n", period_ms);
+
+//New handler for SIGALRM
 struct sigaction new_action;
 memset(&new_action, 0, sizeof(new_action));
 new_action.sa_handler = new_action_funct; 
 sigemptyset( &new_action.sa_mask );
 new_action.sa_flags = 0;
 sigaction(SIGALRM, &new_action, NULL);
-//printf("Defined a new handler for SIGALRM \n");
 
-if (atoi(argv[3]) == 1){
+//Define scheduling policy
+if (strcmp(argv[3],"FIFO")){
+	int priority = atoi(argv[4]);
+	printf("Set SCHED_%s scheduling policy \n", argv[3]);
 	struct sched_param spar;
-	    spar.sched_priority = sched_get_priority_min(SCHED_FIFO) + atoi(argv[4]);
-		printf("Priority of task: %d \n", spar.sched_priority);	
+	spar.sched_priority = sched_get_priority_min(SCHED_FIFO) + priority;	
 	    if(sched_setscheduler(0, SCHED_FIFO, &spar) < 0)
 	    {
 		printf("Unable to set SCHED_FIFO. %d\n", errno);
 		exit(EXIT_FAILURE);
 	    }
 }
-else if(atoi(argv[3]) == 0){
+else if(strcmp(argv[3],"RR")){
+	printf("Set SCHED_%s scheduling policy \n", argv[3]);
 	struct sched_param spar;
-	    spar.sched_priority = sched_get_priority_max(SCHED_RR);
+	spar.sched_priority = sched_get_priority_max(SCHED_RR);
 	    if(sched_setscheduler(0, SCHED_RR, &spar) < 0)
 	    {
 		printf("Unable to set SCHED_RR. %d\n", errno);
 		exit(EXIT_FAILURE);
 	    }
 }
+
 // Setup a signal set for sigwait() to wait for SIGALRM
 sigemptyset(&signal_set);
 sigaddset(&signal_set, SIGALRM);
@@ -81,10 +82,7 @@ sigaddset(&signal_set, SIGALRM);
 timer_settime(timer, 0, &timer_spec, NULL);
 int signum;
 while(1){
-	//sigprocmask(SIG_UNBLOCK, &signal_set, NULL);
-	//printf("Time used for execution is %ld \n", period - timer_gettime(timer, &timer_spec));
 	p_task(period_ms, load, timer, time_left);	
 	sigwait(&signal_set, &signum);
-	
 	}
 }
